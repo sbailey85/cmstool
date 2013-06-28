c39c222790046c32490f1ca906bce83d331bae90
sean <sean.bailey@gmail.com>
2013-06-28 17:58:09 -0500

    stuff

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 __author__ = 'sean'
+import MySQLdb as mysql
 
+class connect:
+    def __init__(self):
+        self.conn = mysql.connect(host = "localhost",user = "test",
+        passwd = "password", db = "test")
+    def kill(self):
+        self.conn.close()
 
+    def nume(self):
+        return self.conn
 
+sql = "select * from wp_users"
+conn = connect.nume()
+cursor = conn.cursor()
+cursor.execute(sql)
+cursor.fetchone()
